a694cc6703300b884cc77d60aed39696c5dbba3d
Gideon Isa <innocentgideon10@gmail.com>
2026-08-13 02:13:45 +0100

    initial commit

---
+++ src/test/java/com/polarisdigitech/boxdeliveryservice/BoxDeliveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polarisdigitech.boxdeliveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoxDeliveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
